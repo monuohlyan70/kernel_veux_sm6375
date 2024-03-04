22a99d2dd9337156a5ba6263f02421270ce0656f
RedEnemy30 <nztmscreempendi@yahoo.co.id>
2024-03-04 14:49:40 +0000

    AnyKernel3 Update

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -2,40 +2,42 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
+# global properties
 properties() { '
-kernel.string=build-alpha kernel by NztmPendi
-do.devicecheck=0
-do.modules=0
-do.systemless=1
+kernel.string=e+ mod kernel by @GrimReaper
+do.devicecheck=1
+device.name1=veux
+device.name2=peux
 do.cleanup=1
-do.cleanuponabort=0
-supported.versions=
-supported.patchlevels=
 '; } # end properties
 
-### AnyKernel install
 override_cmdline() {
-local cmdline='quiet console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x04C8C000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm _rtb.filter= 0x237 service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3 swiotlb=0 loop.max_part=7 cgroup.memory=nokmem,nosocket iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 firmware_class.path=/vendor/firmware'
+local cmdline='androidboot.hardware=qcom lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.usbcontroller=4e00000.dwc3 swiotlb=noforce loop.max_part=7 iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 firmware_class.path=/vendor/firmware buildvariant=user'
 sed -i '/^cmdline/d' $split_img/header;
 echo cmdline=$cmdline >> $split_img/header;
 }
 
-## boot shell variables
+### AnyKernel install
+# boot shell variables
 block=boot;
 is_slot_device=auto;
 no_block_display=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
-ui_print " " "Flashing KernelSU version. You have been warned about security implications.";
+
+if [[ -e $home/KSU_UNLOCK && -f $home/ksu.bdf ]]; then
+    ui_print " " "Flashing KernelSU version. You have been warned about security implications.";
+    $bin/bspatch $home/Image $home/Image_ksu $home/ksu.bdf;
+    mv -f $home/Image_ksu $home/Image;
+fi;
 
 # boot install
 split_boot;
 flash_boot;
 ## end boot install
 
-## vendor_boot shell variables
+# vendor_boot shell variables
 block=vendor_boot;
 is_slot_device=auto;
 
@@ -46,4 +48,5 @@ reset_ak;
 split_boot;
 override_cmdline;
 flash_boot;
-## end vendor_kernel_boot install
+## end vendor_boot install
+
